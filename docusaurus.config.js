6dec6e080a9cc815bc96dc52f8a3630dffd5b4a7
TheeDeer <93623831+TheeDeer0@users.noreply.github.com>
2022-05-04 21:23:14 -0500

    d

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,14 +8,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://scpf.network',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'scp-f', // Usually your GitHub org/user name.
+  projectName: 'Network', // Usually your repo name.
+  trailingSlash: false,
+  
   presets: [
     [
       'classic',
